2ee0cabc5b5602a4b0345553efa88a24c93bb6e8
barrosGuih <cunha.barros.guilherme@gmail.com>
2026-02-15 13:30:02 -0300

    initializing entry and exit for products

---
+++ src/model/Movimentacao.java
@@ -0,0 +1,23 @@
+package src.model;
+
+import java.time.LocalDateTime;
+
+public class Movimentacao {
+    private Produto produto;
+    private Integer quantidade;
+    private String tipo;
+    private LocalDateTime dataHora;
+
+    public Movimentacao (Produto produto, Integer quantidade, String tipo){
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.tipo = tipo;
+        this.dataHora = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString(){
+        return dataHora + " | " + tipo + " | " +  produto.getNome() + " | Quantidade: " + quantidade;
+        
+    }
+}
--- src/service/EstoqueService.java
+++ src/service/EstoqueService.java
@@ -12,5 +12,31 @@ public class EstoqueService {
         produtos.put(produto.getId(), produto);
         System.out.println("Produto adicionado: " + produto.getNome());
     }
+
+    public void entradaProduto(Integer id, Integer quantidade){
+        Produto p = produtos.get(id);
+        if( p != null){
+            p.setQuantidade(p.getQuantidade() + quantidade);
+            historico.add(new Movimentacao(p, quantidade,"Entrada"));
+            System.out.println("Entrada registrada.");
+        } else{
+            System.out.println("Produto nao encontrado");
+        }
+    }
+
+    public void saidaProduto(Integer id, Integer quantidade){
+        Produto p = produtos.get(id);
+        if(p != null){
+            if(p.getQuantidade() >= quantidade) {
+                p.setQuantidade(p.getQuantidade() - quantidade);
+                historico.add(new Movimentacao(p, quantidade, "Saida"));
+                System.out.println("Saida registrada");
+            } else{
+                System.out.println("Quantidade insuficiente em estoque");
+            }   
+        } else{
+            System.out.println("Produto nao encontrado");
+        }
+    }
     
 }
